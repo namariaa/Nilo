eea24e1e1caab86b9071dae73812351ac2036c13
namariaa <anamariafag18@gmail.com>
2025-09-03 16:33:14 -0300

    fix: Fix errors in cmake file. #3

--- src/debug/CMakeFiles/Makefile.cmake
+++ src/debug/CMakeFiles/Makefile.cmake
@@ -8,12 +8,16 @@ set(CMAKE_DEPENDS_GENERATOR "Unix Makefiles")
 set(CMAKE_MAKEFILE_DEPENDS
   "CMakeCache.txt"
   "/home/namaria/Gram-tica-com-ANTLR/src/CMakeLists.txt"
+  "/home/namaria/Gram-tica-com-ANTLR/src/antlr/ExternalAntlr4Cpp.cmake"
+  "/home/namaria/Gram-tica-com-ANTLR/src/antlr/FindANTLR.cmake"
   "CMakeFiles/3.28.3/CMakeCCompiler.cmake"
   "CMakeFiles/3.28.3/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.28.3/CMakeSystem.cmake"
+  "antlr4_runtime/tmp/antlr4_runtime-mkdirs.cmake"
   "/usr/share/cmake-3.28/Modules/CMakeCInformation.cmake"
   "/usr/share/cmake-3.28/Modules/CMakeCXXInformation.cmake"
   "/usr/share/cmake-3.28/Modules/CMakeCommonLanguageInclude.cmake"
+  "/usr/share/cmake-3.28/Modules/CMakeFindJavaCommon.cmake"
   "/usr/share/cmake-3.28/Modules/CMakeGenericSystem.cmake"
   "/usr/share/cmake-3.28/Modules/CMakeInitializeConfigs.cmake"
   "/usr/share/cmake-3.28/Modules/CMakeLanguageInformation.cmake"
@@ -23,6 +27,18 @@ set(CMAKE_MAKEFILE_DEPENDS
   "/usr/share/cmake-3.28/Modules/Compiler/GNU-C.cmake"
   "/usr/share/cmake-3.28/Modules/Compiler/GNU-CXX.cmake"
   "/usr/share/cmake-3.28/Modules/Compiler/GNU.cmake"
+  "/usr/share/cmake-3.28/Modules/ExternalProject.cmake"
+  "/usr/share/cmake-3.28/Modules/ExternalProject/PatchInfo.txt.in"
+  "/usr/share/cmake-3.28/Modules/ExternalProject/RepositoryInfo.txt.in"
+  "/usr/share/cmake-3.28/Modules/ExternalProject/UpdateInfo.txt.in"
+  "/usr/share/cmake-3.28/Modules/ExternalProject/cfgcmd.txt.in"
+  "/usr/share/cmake-3.28/Modules/ExternalProject/download.cmake.in"
+  "/usr/share/cmake-3.28/Modules/ExternalProject/extractfile.cmake.in"
+  "/usr/share/cmake-3.28/Modules/ExternalProject/mkdirs.cmake.in"
+  "/usr/share/cmake-3.28/Modules/ExternalProject/shared_internal_commands.cmake"
+  "/usr/share/cmake-3.28/Modules/FindJava.cmake"
+  "/usr/share/cmake-3.28/Modules/FindPackageHandleStandardArgs.cmake"
+  "/usr/share/cmake-3.28/Modules/FindPackageMessage.cmake"
   "/usr/share/cmake-3.28/Modules/Platform/Linux-GNU-C.cmake"
   "/usr/share/cmake-3.28/Modules/Platform/Linux-GNU-CXX.cmake"
   "/usr/share/cmake-3.28/Modules/Platform/Linux-GNU.cmake"
@@ -39,10 +55,21 @@ set(CMAKE_MAKEFILE_OUTPUTS
 
 # Byproducts of CMake generate step:
 set(CMAKE_MAKEFILE_PRODUCTS
+  "antlr4_runtime/tmp/antlr4_runtime-mkdirs.cmake"
+  "antlr4_runtime/src/antlr4_runtime-stamp/download-antlr4_runtime.cmake"
+  "antlr4_runtime/src/antlr4_runtime-stamp/extract-antlr4_runtime.cmake"
+  "antlr4_runtime/src/antlr4_runtime-stamp/antlr4_runtime-urlinfo.txt"
+  "antlr4_runtime/src/antlr4_runtime-stamp/antlr4_runtime-update-info.txt"
+  "antlr4_runtime/src/antlr4_runtime-stamp/antlr4_runtime-patch-info.txt"
+  "antlr4_runtime/tmp/antlr4_runtime-cfgcmd.txt"
+  "antlr4_runtime/tmp/antlr4_runtime-cache-Debug.cmake"
   "CMakeFiles/CMakeDirectoryInformation.cmake"
   )
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/main.dir/DependInfo.cmake"
+  "CMakeFiles/antlr4_runtime.dir/DependInfo.cmake"
+  "CMakeFiles/antlr4_runtime-build_static.dir/DependInfo.cmake"
+  "CMakeFiles/antlr4_runtime-build_shared.dir/DependInfo.cmake"
+  "CMakeFiles/demo.dir/DependInfo.cmake"
   )
