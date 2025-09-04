47b21e8a58f63cc357aa1d10816108d04dac3720
namariaa <anamariafag18@gmail.com>
2025-09-04 16:47:44 -0300

    ♻️resfactor: Alter cmakefile

---
+++ src/debug/antlr4-cpp-runtime-4.13.2-bin/runtime/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/namaria/Downloads/simple-language/antlr4-cpp-runtime-4.13.2-source/runtime
+# Build directory: /workspaces/namaria/Downloads/simple-language/build/antlr4-cpp-runtime-4.13.2-bin/runtime
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
