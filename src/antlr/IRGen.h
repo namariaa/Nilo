d95217db8bfe0e3f38abb7d970b2676b0aa52045
namariaa <anamariafag18@gmail.com>
2025-12-10 21:31:37 -0300

    ✨feat: Adiciona implementação do visitProgram. #26

--- src/antlr/IRGen.h
+++ src/antlr/IRGen.h
@@ -1,10 +1,56 @@
 #include "NiloScriptVisitor.h"
 #include <stdlib.h>
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/NoFolder.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
 
 using namespace std;
 
-class Visitor : public NiloScriptVisitor{
+unique_ptr<llvm::LLVMContext> Conteiner;
+unique_ptr<llvm::Module> Executable;
+unique_ptr<llvm::IRBuilder<llvm::NoFolder>> Builder;
+
+class IRGen : public NiloScriptVisitor{
+    private:
+        llvm::Function *MainFunction;
+        llvm::BasicBlock *CurrentBasicBlock;
+
+    IRGen(NiloScriptParser::ProgramContext* tree){
+        Conteiner = make_unique<llvm::LLVMContext>();
+        Executable = make_unique<llvm::Module>("NiloScriptModule", *Conteiner);
+        Builder = make_unique<llvm::IRBuilder<llvm::NoFolder>>(*Conteiner);
+
+        //Adiciona informações de configurações para executável
+        Executable->setDataLayout(llvm::DataLayout("e-m:e-i64:64-f80:128-n8:16:32:64-S128"));
+        Executable->setModuleIdentifier("Executável");
+        Executable->setTargetTriple("x86_64-unknown-linux-gnu");
+        
+        //Cria função main
+        llvm::FunctionType *MAINTYPE = llvm::FunctionType::get(llvm::Type::getInt32Ty(*Conteiner), false);
+        MainFunction = llvm::Function::Create(MAINTYPE, llvm::Function::ExternalLinkage, "main", Executable.get());
+        CurrentBasicBlock = llvm::BasicBlock::Create(*Conteiner, "entrada", MainFunction);
+        Builder->SetInsertPoint(CurrentBasicBlock);
+        
+        //Inicia visitação da árvore para gerar executável 
+        visitProgram(tree);
+    }
+
     virtual std::any visitProgram(NiloScriptParser::ProgramContext *context) override {
+        llvm::Value *result = nullptr;
+
+        for(int i = 0; i < context->children.size(); i++){
+            any response = visitStmt(context->stmt()[i]);
+        }
+
         cerr << "A funcionalidade não está implementada ainda, desculpe o transtorno!" << endl;
         exit(1);
     };
